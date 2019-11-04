2eb3aa187928573938694b3f526eb5db53d5dcdf
MarioDena <enrique.dn@outlook.com>
2019-11-04 13:15:29 -0600

    added methods for select all?, any? and none? also added test cases

--- enumerables.rb
+++ enumerables.rb
@@ -18,8 +18,56 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_select
+    i = 0
+    ar = self
+    array = []
+    while i < ar.size
+      array << ar[i] if yield(ar[i])
+      i += 1
+    end
+    array
+  end
+
+  def my_all?
+    i = 0
+    ar = self
+    while i < ar.size
+      return false unless yield(ar[i])
+
+      i += 1
+    end
+    true
+  end
+
+  def my_any?
+    i = 0
+    ar = self
+    while i < ar.size
+      return true if yield(ar[i])
+
+      i += 1
+    end
+    false
+  end
+
+  def my_none?
+    i = 0
+    ar = self
+    while i < ar.size
+      return false if yield (ar[i])
+
+      i += 1
+    end
+    true
+  end
 end
 
 # Test cases
 [1, 2, 3, 4].my_each { |_x| puts "working #{_x}" }
-[1, 2, 3, 4].my_each_index { |x, y| puts "#{x} at index #{y}" }
+[1, 2, 3, 4].my_each_with_index { |x, y| puts "#{x} at index #{y}" }
+p [1, 2, 3, 4].my_select(&:even?)
+p %w[testing this theme].my_all? { |x| x[0] == 't' }
+p %w[testing this theme].my_any? { |x| x[0] == 'x' }
+p %w[testing this theme].my_none? { |x| x[0] == 'x' }
